dd88df7b392d2f82e2dab1fd136651a87e6186ee
Ryan Murphy <Ryan.Murphy12@gmail.com>
2022-12-27 11:29:20 -0500

    First Commit

---
+++ CurrentGameLines.py
@@ -0,0 +1,17 @@
+
+
+
+#Obtain a list of current games
+#Obtain a list of upcoming games
+#Keep these lists separate
+#Combine these lists just to get the ID of the contest
+#Create a numerical ID for all games live and upcomming
+
+#Create an object that
+#  selects the current game
+#  selects the number of pulls
+#  selects the time delay
+
+#
+
+
