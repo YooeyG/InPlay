92b7512ae991dbfbe61c6c2069e2a1e33cdabb2e
Ryan Murphy <Ryan.Murphy12@gmail.com>
2022-12-28 10:12:27 -0500

    Testing

---
+++ pickle-sampleData.py
@@ -0,0 +1,10 @@
+#Import the pickle settings, pandas and other API data
+# Determine the saved hard drive location of the data
+
+#Data will need to be repetitive game data,
+#New games coming in
+#Old games dropping
+
+#quick pandas sectioning off
+
+print("hello")
